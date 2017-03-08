ed4525500559a9bae9540f4d1ff85a29d55f4806
allyshsaye <allisonshaye@gmail.com>
2017-03-08 15:07:08 -0800

    Write migration file for Tournaments

--- bracket_maker/app/models/tournament.rb
+++ bracket_maker/app/models/tournament.rb
@@ -1,4 +1,11 @@
 class Tournament < ApplicationRecord
+  has_many :rounds
+  has_many :games, through: :rounds
+  has_many :teams, through: :games
+  belongs_to :admin, class_name: "User"
+
+  validates :name, :event, :admin_id, presence: true
+  validates_associated :rounds
 
 end
 
