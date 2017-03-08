2943812e877b3a089e483d5a9748dd5c61012c44
allyshsaye <allisonshaye@gmail.com>
2017-03-08 15:16:22 -0800

    Add 'recent' scope to Tournament class

--- bracket_maker/app/models/tournament.rb
+++ bracket_maker/app/models/tournament.rb
@@ -5,7 +5,10 @@ class Tournament < ApplicationRecord
   belongs_to :admin, class_name: "User"
 
   validates :name, :event, :admin_id, presence: true
+  
   validates_associated :rounds
+
+  scope :recent, -> { order("created_at DESC").limit(6) }
 
 end
 
