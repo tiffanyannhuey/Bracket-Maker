b5daa48baab81a026bf87f4a8f6015627b8fa763
Tiffany Ann Huey <email": null>
2017-03-08 20:06:18 -0800

    add session helpers

--- bracket_maker/app/controllers/users_controller.rb
+++ bracket_maker/app/controllers/users_controller.rb
@@ -3,9 +3,21 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new()
+    render plain: params[:user].inspect
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to '/user'
+    else
+      render 'new'
+    end
   end
 
   def show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
