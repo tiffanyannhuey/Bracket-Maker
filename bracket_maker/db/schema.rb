9005e663949dddb48a19d9664aae45f817aea2d9
Tiffany Ann Huey <email": null>
2017-03-08 19:55:17 -0800

    update order of migration

--- bracket_maker/db/schema.rb
+++ bracket_maker/db/schema.rb
@@ -10,12 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170308233401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "game_teams", force: :cascade do |t|
     t.integer  "game_id"
@@ -42,5 +40,4 @@ ActiveRecord::Schema.define(version: 20170308233401) do
 
   add_foreign_key "game_teams", "games"
   add_foreign_key "game_teams", "teams"
-
 end
