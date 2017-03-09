74b88b1bd13be369be524fd9beb1f19081d7a128
Brendan Schwarz <bschwarz890@gmail.com>
2017-03-08 20:26:25 -0800

    validations

--- bracket_maker/app/models/tournament.rb
+++ bracket_maker/app/models/tournament.rb
@@ -2,15 +2,13 @@ class Tournament < ApplicationRecord
   has_many :rounds
   has_many :games, through: :rounds
   has_many :teams, through: :games
-  belongs_to :admin, class_name: "User"
+  belongs_to :admin, { class_name: "User" }
 
   validates :name, :event_type, :admin_id, presence: true
 
   validates :completed, inclusion: { in: [true, false] }
 
   validates_associated :rounds, :games, :teams
-
-  validates :admin, absence: true
 
   scope :recent, -> { order("created_at DESC").limit(6) }
 
