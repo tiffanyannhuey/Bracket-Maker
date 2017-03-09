a4b1816f36ec9b3c54cc0a97c35487e2d302b291
Brendan Schwarz <bschwarz890@gmail.com>
2017-03-09 08:02:47 -0800

    more associations shenanigans

--- bracket_maker/db/migrate/20170309034604_create_rounds.rb
+++ bracket_maker/db/migrate/20170309034604_create_rounds.rb
@@ -1,7 +1,7 @@
 class CreateRounds < ActiveRecord::Migration[5.0]
   def change
     create_table :rounds do |t|
-      t.integer :game_id, null:false
+      t.integer :tournament_id, null:false
       t.integer :number, null:false
 
       t.timestamps
--- bracket_maker/spec/models/game_spec.rb
+++ bracket_maker/spec/models/game_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
+  user = User.create(username: "test", email: "test@test.com", password: "test")
+  tournament = user.tournaments.create
 
+  describe "associations" do
+    it "has_many teams through game_teams" do
+
+    end
+  end
 end
