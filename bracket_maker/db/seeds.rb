6fb2a280eaef7eb6e27c774aaab8b45e15d36862
Brendan Schwarz <bschwarz890@gmail.com>
2017-03-09 15:10:50 -0800

    rspec game model

--- bracket_maker/db/seeds.rb
+++ bracket_maker/db/seeds.rb
@@ -8,6 +8,3 @@
 User.first.tournaments.create!(name: "All Star Basketball Tournament", event_type: "Basketball", teams: 6)
 User.second.tournaments.create!(name: "Vavi Volleyball tournaments", event_type: "Volleyball", teams: 15)
 User.last.tournaments.create!(name: "Cornhole Championship", event_type: "Cornhole", teams: 18)
-
-
-Tournaments.first.games.cr
--- bracket_maker/spec/models/game_spec.rb
+++ bracket_maker/spec/models/game_spec.rb
@@ -1,12 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  user = User.create(username: "test", email: "test@test.com", password: "test")
-  tournament = user.tournaments.create
+  describe "attributes" do
+    it "has a position within a round" do
+      round_1 = Round.create(tournament_id: 1, number: 1)
+      round_1.games.push(3.times.with_object([]) {|position, collection| collection << Game.create(position: position + 1)})
+      expect(round_1.games.first.position).to eq 1
+      expect(round_1.games.second.position).to eq 2
+      expect(round_1.games.third.position).to eq 3
+    end
+  end
 
   describe "associations" do
-    it "has_many teams through game_teams" do
+    it { should have_many(:game_teams) }
+    it { should belong_to(:round) }
+    it { should have_one(:tournament) }
+    it { should have_one(:admin) }
+    it { should have_many(:teams) }
+  end
 
-    end
+  describe "validations" do
+    it { should validate_presence_of(:position) }
   end
 end
