35f6be0b8ae94e86e44c4a93d4028c9415481a1c
allyshsaye <allisonshaye@gmail.com>
2017-03-09 08:33:25 -0800

    Resolve conflicts

--- bracket_maker/db/seeds.rb
+++ bracket_maker/db/seeds.rb
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-Tournament.create!(name: "All Star Basketball Tournament", event: "Basketball", admin_id: 1)
-Tournament.create!(name: "Vavi Volleyball Tournament", event: "Volleyball", admin_id: 2)
-Tournament.create!(name: "Cornhole Championship", event: "Cornhole", admin_id: 3)
-=======
 users = []
 5.times { users << User.create!(
     username: Faker::Internet.user_name(Faker::HarryPotter.character),
@@ -13,5 +8,5 @@ users = []
 Tournament.create!(name: "All Star Basketball Tournament", event_type: "Basketball", admin_id: users[0].id)
 Tournament.create!(name: "Vavi Volleyball Tournament", event_type: "Volleyball", admin_id: users[1].id)
 Tournament.create!(name: "Cornhole Championship", event_type: "Cornhole", admin_id: users[2].id)
->>>>>>> 9db4ff4491b7a81a7293ea941135278be83f1df9
+
 
