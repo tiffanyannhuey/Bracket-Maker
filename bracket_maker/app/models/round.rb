2e3c784b7af01c76214d6a976104dfd092ee56a4
Brendan Schwarz <bschwarz890@gmail.com>
2017-03-10 14:19:36 -0800

    winner implemented

--- bracket_maker/app/models/round.rb
+++ bracket_maker/app/models/round.rb
@@ -5,9 +5,15 @@ class Round < ApplicationRecord
   has_many :game_teams, through: :games
   has_many :teams, through: :games
 
+  # after_update :advance_lonely_player
+
   validates :number, presence: true
 
-  validates_associated :games, :teams, :tournament
+  # validates_associated :games, :teams, :tournament
 
   validates_presence_of :admin
+
+  # def advance_lonely_player
+  #   self.games.find { |game| game.teams.count == 1 }.game_teams.first.advance_player
+  # end
 end
