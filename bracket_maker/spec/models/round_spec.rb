99f2968ede5351186df3a4504a82da5cc4344d41
allyshsaye <allisonshaye@gmail.com>
2017-03-09 16:20:55 -0800

    Add shoulda-matchers for Round rspec

--- bracket_maker/spec/models/round_spec.rb
+++ bracket_maker/spec/models/round_spec.rb
@@ -8,6 +8,17 @@ RSpec.describe Round, type: :model do
     it { should have_many(:games) }
 
     it { should have_many(:teams) }
+
+    it { should have_one(:admin) }
+
+    it { should belong_to(:tournament) }
+
+    round_columns = [:id, :number, :tournament_id]
+
+    round_columns.each do |column|
+      it { should have_db_column(column) }
+    end
+
   end
 end
 
