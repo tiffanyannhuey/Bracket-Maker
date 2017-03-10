35b5e4de10ed340aec2d302a4d329db823c21714
Tiffany Ann Huey <email": null>
2017-03-09 18:02:56 -0800

    pass more tests

--- bracket_maker/spec/controllers/sessions_controller_spec.rb
+++ bracket_maker/spec/controllers/sessions_controller_spec.rb
@@ -15,20 +15,46 @@ RSpec.describe SessionsController, type: :controller do
   end
 
   describe "POST #create" do
+    before(:all) { User.create(username: 'Bill', email: 'bill@aol.com', password: 'ducktyping') }
     context "when params are valid" do
-      it "responds with status code of 200"
-      it "create a new session"
-      it "redirects to the homepage"
-    end
+      subject(:post_request) {post :create, params: { session: { email: 'bill@aol.com', password: "ducktyping"} } }
+
+      it "responds with status code of 302" do
+        # p post_request.inspect
+        post_request
+        expect(response).to have_http_status 302
+      end
+
+      # it "create a new session" do
+      #   post_request
+      #   expect(assigns()).to eq
+
+      # end
+
+      it "assigns the user as @user" do
+        post_request
+        expect(assigns(:user)).to eq(User.last)
+      end
+
+      it "redirects to the profile page" do
+        post_request
+        expect(response).to redirect_to user_path(User.last.id)
+      end
 
     context "when params are invalid" do
+      subject(:bad_post_request) {post :create, params: { session: { email: 'bill@aol.com', password: "no"} } }
+      it "responds with a status code of 401" do
+        bad_post_request
+        expect(response).to have_http_status 401
+      end
       it "sets a notice that the login failed"
       it "renders the :new template"
     end
+
   end
 
-  describe "DELETE #destroy" do
-    it "sets the session as nil"
+#   describe "DELETE #destroy" do
+#     it "sets the session as nil"
   end
 
 end
--- bracket_maker/spec/controllers/user_controller_spec.rb
+++ bracket_maker/spec/controllers/user_controller_spec.rb
@@ -16,19 +16,23 @@ RSpec.describe UsersController, type: :controller do
 
   describe "POST #create" do
     context "when valid params are passed" do
-      subject(:valid_user) {post :create, user: {username: "Todd", email: "123@abc.com", password: "password"}}
+      subject(:post_request) {post :create, params: { user: { username: "Todd", email: "123@abc.com", password: "password"} } }
+
       it "responds with status code 302" do
-        valid_user
+        # valid_user = double("user", :username => "Tom", :email => "tom@gmail.com", :password => "password")
+        # post :create, params: { user: {username: "Todd", email: "123@abc.com", password: "password"} }
+        post_request
         expect(response).to have_http_status 302
       end
 
-      # it "creates a new user in the database" do
-      #   expect { valid_user }.to change {User.count}.by(1)
-      # end
+      it "creates a new user in the database" do
+        expect{ post_request }.to change{User.count}.by(1)
+      end
 
-      # it "assigns the newly created user as @user" do
-      #   expect(assigns(:user)).to eq(User.last)
-      # end
+      it "assigns the newly created user as @user" do
+        post_request
+        expect(assigns(:user)).to eq(User.last)
+      end
 
     end
   end
